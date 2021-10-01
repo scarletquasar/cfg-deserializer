f82516522bb7904167594d9b9341ad8a9b3127ad
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-10-01 08:52:33 -0300

    DevUpdate

--- CfgSerializer.cs
+++ CfgSerializer.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections.Generic;
-
 using System;
 
 namespace CfgManagement
 {
     public class CfgSerializer
     {
+        public static string Serialize(Dictionary<string,string> target) {
+            string result = string.Empty;
+            foreach(var key in target.Keys) {
+                result += key + "=" + target[key];
+            }
+            return result;
+        }
     }
 }
